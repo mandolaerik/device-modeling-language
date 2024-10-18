e0ff61b28dd6f6611f96455aa310715d5aa14001
Erik Carstensen <erik.carstensen@intel.com>
2024-10-18 14:46:52 +0200

    Create provisional and compat features around the `vect` type

--- py/dml/messages.py
+++ py/dml/messages.py
@@ -1891,6 +1891,12 @@ class EEXTERNINCOMP(DMLError):
             "conflicting declaration, which specifies the type: "
             + self.other_type.describe())
 
+
+class ECVECT(DMLError):
+    """`vect` types are only permitted if the [`c_vect` provisional
+    feature](provisional-auto.html#c_vect) is enabled."""
+    fmt = "declaration of vect type without c_vect provisional"
+
 #
 # WARNINGS (keep these as few as possible)
 #
