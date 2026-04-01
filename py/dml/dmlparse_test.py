6bb93119805052a25a5d7ea47ebefadf5754781c
Erik Carstensen <erik.carstensen@intel.com>
2026-04-01 08:51:49 +0200

    Tighten error condition
    Fixup function should not be called on nonempty rules
    
    Also rewrite as unittest

--- py/dml/dmlparse_test.py
+++ py/dml/dmlparse_test.py
@@ -6,24 +6,6 @@ import unittest
 import re
 import sys
 
-empty_prod_re = re.compile(r'[:|]\s*(?:$|\|)')
-
-bad_emptyprod_rules = set()
-for rules in (dmlparse.production_rules_dml12,
-              dmlparse.production_rules_dml14):
-    for (name, rule) in rules.items():
-        if (name != 'p_error' and empty_prod_re.search(rule.__doc__)
-            and 'fixup_emptyprod_lexpos' not in rule.__code__.co_names):
-            bad_emptyprod_rules.add(rule)
-
-if bad_emptyprod_rules:
-    sys.stderr.write(
-        "empty production rule(s) without call to 'fixup_emptyprod_lexpos':\n")
-    for rule in bad_emptyprod_rules:
-        sys.stderr.write(f'    {rule.__name__}\n')
-
-    sys.exit(1)
-
 def parse(contents):
     file_info = logging.FileInfo(
         '<unit test>', (1, 4), content_lines=contents.splitlines(
@@ -33,6 +15,21 @@ def parse(contents):
     return ast
 
 class test_emptyprod_based_sites(unittest.TestCase):
+    def test_calls_from_empty_rules(self):
+        empty_prod_re = re.compile(r'[:|]\s*(?:$|\|)')
+        assert empty_prod_re.search('foo : \n')
+        assert not empty_prod_re.search('foo : something\n')
+
+        for rules in (dmlparse.production_rules_dml12,
+                      dmlparse.production_rules_dml14):
+            for (name, rule) in rules.items():
+                if name != 'p_error':
+                    self.assertEqual(
+                        bool(empty_prod_re.search(rule.__doc__)),
+                        'fixup_emptyprod_lexpos' in rule.__code__.co_names,
+                        f"bad production rule {name}: need fix_emptyprod_lexpos"
+                        " call if and only if a production rule is empty")
+
     def test(self):
         # Test that sites are actually fixed up by fixup_emptyprod_lexpos.
         # Without it, sites would be ruined by empty production rules.
